5d45c14bd82adf8bcdad5b8242a8cabba50fb1d5
aspalladan <engrdanalupalladan@gmail.com>
2023-12-30 22:06:50 +0100

    Day 15 python error types

---
+++ 015_Day_Python_type_errors/python_type_errors_day_15.py
@@ -0,0 +1,20 @@
+# Day 15: 30 days of Python programming
+
+
+'''
+Practised the different Error types on the Python interactive shell. The errors included:
+1. SyntaxError
+2. NameError
+3. IndexError
+4. ModuleNotFoundError
+5. AttributeError
+6. KeyError
+7. TypeError
+8. ImportError
+9. ValueError
+10. ZeroDivisionError
+
+
+
+Errors will definitely be encountered as we continue the programming journey and handling them, will from now on, be more intuitive. 
+'''
